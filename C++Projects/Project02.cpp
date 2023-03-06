d802fe223b325661f085f9d97642e847ec3a8554
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-03-06 13:53:11 +0500

    Adding my old c and C++ learning code

---
+++ C++Projects/Project02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class name{
+    String "fisrtname","lastname";
+    public:
+        void Sname(void){
+            firstname = a;
+            lastname = b;
+
+        } 
+}
+
+int main(){
+    name muhsan;
+    
+    muhsan.Sname("Muhsan", "Javed");
+
+    
+    return 0;
+}
